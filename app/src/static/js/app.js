ef573e8d1b2d896c4574bc8f06288f68aa1680c3
Christopher John <Christopher.John-CIC.Uk@ibm.com>
2021-10-15 13:33:34 +0100

    Various updates

--- app/src/static/js/app.js
+++ app/src/static/js/app.js
@@ -53,7 +53,7 @@ function TodoListCard() {
         <React.Fragment>
             <AddItemForm onNewItem={onNewItem} />
             {items.length === 0 && (
-                <p className="text-center">Nothing here! Want to add something?</p>
+                <p className="text-center">List is empty. Lets add something!</p>
             )}
             {items.map(item => (
                 <ItemDisplay
